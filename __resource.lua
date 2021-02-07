639915e3dfb9fdb25d979f9cc8fffb9ce8f61dac
Foaf <56663724+foaf0@users.noreply.github.com>
2021-02-06 19:42:25 -0800

    Fixed a bug

--- __resource.lua
+++ __resource.lua
@@ -3,7 +3,6 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 client_scripts {
 	'UltimateLogger/foaf_ultimate.lua',
 	'UltimateLogger/foaf_ultimateip.lua',
-	'config.lua',
 }
 
 server_scripts {
